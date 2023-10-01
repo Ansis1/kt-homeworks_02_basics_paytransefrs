0cff73b3d9f4e7d2ef3158b0b7ee53b37155cce0
Ansis Kalnyn <ansis.kalnyn@yandex.ru>
2023-10-01 19:21:39 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+import java.text.DecimalFormat
+
+fun main() {
+
+    val amount = 15023
+    val taxRate = 0.075
+    val taxMin = 35
+    val commissionCalc = amount * taxRate
+    val commission = if (commissionCalc > 35) commissionCalc else taxMin
+    val numberView =  DecimalFormat("#.##").format(commission);
+    println("Комиссия за перевод: $amount р. составит: $numberView р.")
+}
