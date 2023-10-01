122ae1b91b83dfef875da50f14bbe5859524b63e
Ansis Kalnyn <ansis.kalnyn@yandex.ru>
2023-10-01 19:56:28 +0300

    format main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,6 +7,6 @@ fun main() {
     val taxMin = 35
     val commissionCalc = amount * taxRate
     val commission = if (commissionCalc > 35) commissionCalc else taxMin
-    val numberView =  DecimalFormat("#.##").format(commission);
+    val numberView = DecimalFormat("#.##").format(commission);
     println("Комиссия за перевод: $amount р. составит: $numberView р.")
 }
